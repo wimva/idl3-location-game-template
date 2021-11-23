5afedd89e4baef1dd5e267b96bf73679a8452948
Wim Van Aerschot <>
2021-11-23 15:49:35 +0100

    update camera plugin

--- scripts/camera.js
+++ scripts/camera.js
@@ -1,23 +1,61 @@
-let videoElement = null;
-let canvasElement = null;
-let captureButton = null;
+async function startCamera(useFrontCamera, videoSelector, canvasSelector, captureSelector, onClickFunction) {
+  let videoElement = document.querySelector(videoSelector);
+  let canvasElement = document.querySelector(canvasSelector);
+  let captureButton = document.querySelector(captureSelector);
 
-async function startCamera(videoSelector, canvasSelector, captureSelector, onClickFunction) {
-  videoElement = document.querySelector(videoSelector);
-  canvasElement = document.querySelector(canvasSelector);
-  captureButton = document.querySelector(captureSelector);
+  if (!videoElement) console.error('Video selector could not find a valid element');
 
-  if (!videoElement) console.error('camera.js: Video selector could not find a valid element');
-  if (!canvasElement) console.error('camera.js: Canvas selector could not find a valid element');
-  if (!captureButton) console.error('camera.js: Capture Button selector could not find a valid element');
+  let videoConfiguration = { facingMode: { exact: "environment" } };
+  if (useFrontCamera) videoConfiguration = { facingMode: "user" };
 
-  let stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
+  let stream = await navigator.mediaDevices.getUserMedia({ video: videoConfiguration, audio: false });
 	videoElement.srcObject = stream;
 
-  captureButton.onclick = () => {
-    canvasElement.getContext('2d').drawImage(videoElement, 0, 0, canvasElement.width, canvasElement.height);
-    let imageBase64 = canvasElement.toDataURL('image/jpeg');
+  if (captureButton && canvasElement) {
+    captureButton.onclick = () => {
+      canvasElement.width = videoElement.videoWidth;
+      canvasElement.height = videoElement.videoHeight;
+      canvasElement.getContext('2d').drawImage(videoElement, 0, 0);
+      let imageBase64 = canvasElement.toDataURL('image/jpeg');
 
-    if (onClickFunction) onClickFunction(imageBase64);
-  };
+      if (onClickFunction) onClickFunction(imageBase64);
+    };
+  }
 };
+
+/*
+
+Gebruik:
+
+De video tag dient om je beeld te laten zien. Als je een foto wil laten
+nemen heb je de capture button nodig en een canvas element.
+In het canvas element wordt de effectief genomen foto weergegeven.
+
+Om alles op te starten gebruik je de startCamera functie met:
+Eerste argument: true voor front camera, false voor back camera (boolean, required)
+Tweede argument: selector voor het video element (string, required)
+Derde argument: selector voor het canvas element (string, optioneel)
+Vierde argument: selector voor het button element (string, optioneel)
+Vijfde argument: een functie die wordt aangeroepen als er een foto genomen is (function, optioneel)
+
+
+//
+// .html
+//
+
+<video id="video" width="320" height="240" autoplay playsinline></video>
+<button id="capture">Capture</button>
+<canvas id="canvas"></canvas>
+
+//
+// .js
+//
+
+function callback(base64) {
+  console.log('the image was captured');
+  console.log(base64);
+}
+
+startCamera(false, '#video', '#canvas', '#capture', callback);
+
+*/
