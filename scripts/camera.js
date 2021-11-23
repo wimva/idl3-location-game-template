b57c3a93c5da291ec76317b7f98dc47101189316
Wim Van Aerschot <>
2021-11-23 16:18:39 +0100

    put documentation on top of plugins

--- scripts/camera.js
+++ scripts/camera.js
@@ -1,33 +1,3 @@
-async function startCamera(useFrontCamera, videoSelector, canvasSelector, captureSelector, onClickFunction) {
-  let videoElement = document.querySelector(videoSelector);
-  let canvasElement = document.querySelector(canvasSelector);
-  let captureButton = document.querySelector(captureSelector);
-
-  if (!videoElement) console.error('Video selector could not find a valid element');
-
-  let videoConfiguration = { facingMode: { exact: "environment" } };
-  if (useFrontCamera) videoConfiguration = { facingMode: "user" };
-
-  try {
-    let stream = await navigator.mediaDevices.getUserMedia({ video: videoConfiguration, audio: false });
-    videoElement.srcObject = stream;
-
-    if (captureButton && canvasElement) {
-      captureButton.onclick = () => {
-        canvasElement.width = videoElement.videoWidth;
-        canvasElement.height = videoElement.videoHeight;
-        canvasElement.getContext('2d').drawImage(videoElement, 0, 0);
-        let imageBase64 = canvasElement.toDataURL('image/jpeg');
-
-        if (onClickFunction) onClickFunction(imageBase64);
-      };
-    }
-  } catch(e) {
-    console.error(e);
-    alert(e);
-  }
-};
-
 /*
 
 Gebruik:
@@ -64,3 +34,32 @@ function callback(base64) {
 startCamera(false, '#video', '#canvas', '#capture', callback);
 
 */
+
+async function startCamera(useFrontCamera, videoSelector, canvasSelector, captureSelector, onClickFunction) {
+  let videoElement = document.querySelector(videoSelector);
+  let canvasElement = document.querySelector(canvasSelector);
+  let captureButton = document.querySelector(captureSelector);
+
+  if (!videoElement) console.error('Video selector could not find a valid element');
+
+  let videoConfiguration = { facingMode: { exact: "environment" } };
+  if (useFrontCamera) videoConfiguration = { facingMode: "user" };
+
+  try {
+    let stream = await navigator.mediaDevices.getUserMedia({ video: videoConfiguration, audio: false });
+    videoElement.srcObject = stream;
+
+    if (captureButton && canvasElement) {
+      captureButton.onclick = () => {
+        canvasElement.width = videoElement.videoWidth;
+        canvasElement.height = videoElement.videoHeight;
+        canvasElement.getContext('2d').drawImage(videoElement, 0, 0);
+        let imageBase64 = canvasElement.toDataURL('image/jpeg');
+
+        if (onClickFunction) onClickFunction(imageBase64);
+      };
+    }
+  } catch(e) {
+    alert(e);
+  }
+};
