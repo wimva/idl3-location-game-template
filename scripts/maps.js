3422c985b3ecad704998f96cb5737b259017cc9b
Wim Van Aerschot <>
2021-11-26 14:56:00 +0100

    fix: return map with creation and pass when adding a marker

--- scripts/maps.js
+++ scripts/maps.js
@@ -3,22 +3,24 @@ function createMap(myID, lat, lng, zoom, styled) {
     lng = typeof lng !== "undefined" ? lng : 4.411694;
     zoom = typeof zoom !== "undefined" ? zoom : 15;
     styled = typeof styled !== "undefined" ? styled : "mapbox://styles/mapbox/streets-v11";
-  
+
     mapboxgl.accessToken = mapboxAccessToken;
-  
+
     const startCoordinates = {
       lat: lat,
       lng: lng,
     };
-  
+
     const map = new mapboxgl.Map({
       container: myID,
       style: styled,
       center: [startCoordinates.lng, startCoordinates.lat],
       zoom: zoom
     });
+
+    return map;
   }
-  function createMarker(className, myWidth, myHeight, image, lat, lng, draggable) {
+  function createMarker(map, className, myWidth, myHeight, image, lat, lng, draggable) {
     draggable = typeof draggable !== "undefined" ? draggable : false;
     const el = document.createElement("div");
     el.className = className;
@@ -26,11 +28,11 @@ function createMap(myID, lat, lng, zoom, styled) {
     el.style.width = `${myWidth}px`;
     el.style.height = `${myHeight}px`;
     el.style.backgroundSize = "100%";
-    
+
     const marker = new mapboxgl.Marker(el, {
       draggable: draggable
     }).setLngLat([lng,lat]).addTo(map);
   }
-  
-  createMap("map", 51.219608, 4.411694, 10, 'mapbox://styles/matthekim/ckv6gc88y0c9u15t5ppk1horn');
-  createMarker('marker',20, 30, "https://driesvannoten-la.com/img/pin.svg",51.219608,4.411694, false);
+
+  const myMap = createMap("map", 51.219608, 4.411694, 10, 'mapbox://styles/matthekim/ckv6gc88y0c9u15t5ppk1horn');
+  createMarker(myMap, 'marker',20, 30, "https://driesvannoten-la.com/img/pin.svg",51.219608,4.411694, false);
