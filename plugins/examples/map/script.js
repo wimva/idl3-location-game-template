bc60daba9452c6fce320d660c766c7d8c2d1349f
Wim Van Aerschot <wim@vaw.be>
2022-10-17 22:58:17 +0200

    updated map example

--- plugins/examples/map/script.js
+++ plugins/examples/map/script.js
@@ -1,2 +1,2 @@
 const myMap = createMap("mapper", 51.219608, 4.411694, 15, 'mapbox://styles/mapbox/streets-v11');
-createMarker(myMap, 'markerID1', 20, 30, "https://driesvannoten-la.com/img/pin.svg",51.219608, 4.411694, false);
+createMarker(myMap, 'markerID1', 20, 20, "https://vaw.be/cacher/pin-416.png",51.219608, 4.411694, false);
